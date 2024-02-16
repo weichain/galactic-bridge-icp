3cd0dfe86122902b1dfb6f1dda0f496764285a4d
Slav Keremidchiev <skeremidch@gmail.com>
2024-02-16 17:05:19 +0200

    feat: initial

---
+++ scripts/deploy_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+dfx canister create ledger
+dfx canister create minter
+
+dfx deploy ledger --upgrade-unchanged --argument "(variant {
+  Init = record {
+    minting_account = record {
+      owner = principal \"$OWNER_PRINCIPAL\"
+    };
+    feature_flags  = opt record { icrc2 = true };
+    decimals = opt 18;
+    max_memo_length = opt 80;
+    transfer_fee = 0;
+    token_symbol = \"ckSol\";
+    token_name = \"Chain key Solana\";
+    metadata = vec {};
+    initial_balances = vec {};
+    archive_options = record {
+      num_blocks_to_archive = 1000;
+      trigger_threshold = 2000; 
+      max_message_size_bytes = null;
+      cycles_for_archive_creation = opt 1_000_000_000_000;
+      node_max_memory_size_bytes = opt 3_221_225_472;
+      controller_id = principal \"$OWNER_PRINCIPAL\";
+    }
+  }
+})"
+
+
+# dfx deploy minter --argument '(variant {
+#   InitArg = record {
+#     ethereum_network = variant {Sepolia};
+#     ecdsa_key_name = "key_1";
+#     ethereum_contract_address = opt "CONTRACT_ADDRESS";
+#     ledger_id = principal "'"$(dfx canister id ledger)"'";
+#     ethereum_block_height = variant {Finalized};
+#     minimum_withdrawal_amount = 10_000_000_000_000_000;
+#     next_transaction_nonce = NEXT_NONCE
+#   }
+# })'
---
+++ scripts/export.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Export the variables
+export OWNER_PRINCIPAL="hc2qf-bkze5-hncw3-da47r-knj7g-2ghzl-zfyl6-sxxd7-bovrx-zr667-jqe"
+export USER_PRINCIPAL="svq52-4c5cd-olo3w-r6b37-jizpw-kixdx-uarhl-nolu3-gcikk-nza7z-yae"
+export USER_PRINCIPAL_NAME="TestICRC1"
+
+echo "Owner principal: $OWNER_PRINCIPAL"
+echo "User principal: $USER_PRINCIPAL"
+echo "User principal name: $USER_PRINCIPAL_NAME"
