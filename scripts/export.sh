cfc53f4ae9099d04e612e0bba73beda6865bf522
Slav Keremidchiev <skeremidch@gmail.com>
2024-02-29 17:51:02 +0200

    feat: ipdate documentation

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Export the variables
-export OWNER_PRINCIPAL="hc2qf-bkze5-hncw3-da47r-knj7g-2ghzl-zfyl6-sxxd7-bovrx-zr667-jqe"
-export USER_PRINCIPAL="svq52-4c5cd-olo3w-r6b37-jizpw-kixdx-uarhl-nolu3-gcikk-nza7z-yae"
-export USER_PRINCIPAL_NAME="TestICRC1"
+export OWNER_PRINCIPAL=""
+export USER_PRINCIPAL=""
+export USER_PRINCIPAL_NAME=""
 
 echo "Owner principal: $OWNER_PRINCIPAL"
 echo "User principal: $USER_PRINCIPAL"
